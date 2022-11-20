012cfb3bb23da0b1f83c1b0b0e305fcfb8e8471a
Muhammad Rayhan Fadillah <fadillah@suitmedia.com>
2022-11-20 13:10:40 +0700

    api sederhana dengan express

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-const http = require('http')
+const app = require('./server')
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/' && req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify({ message: 'Hello World' }))
-
-    res.end()
-    return
-  }
-
-  res.writeHead(404, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify({ message: 'Not Found' }))
-})
-
-const PORT = process.env.PORT || 9999
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+app.listen(9999, () => {
+  console.log('Server listening on port 9999')
 })
---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 9999
+const path = require('path')
+
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+  res.status(200)
+  // res.json({ message: 'Hello' })
+  res.sendFile(path.resolve('src/pages/index.html'))
+})
+
+module.exports = app
