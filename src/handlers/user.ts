ef8d0cb4533cbe9ba873628a6dac49ce5767e060
Muhammad Rayhan Fadillah <fadillah@suitmedia.com>
2022-12-05 20:08:51 +0700

    Create user.ts

---
+++ src/handlers/user.ts
@@ -0,0 +1,37 @@
+import prisma from '../db'
+import { createJWT, hashPassword, comparePasswords } from '../modules/auth/auth'
+
+export const createNewUser = async (req, res) => {
+  const hash = await hashPassword(req.body.password)
+
+  const user = await prisma.user.create({
+    data: {
+      username: req.body.username,
+      password: hash,
+    },
+  })
+
+  const token = createJWT(user)
+  res.json({ token })
+}
+
+export const signIn = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+    },
+  })
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  const valid = await comparePasswords(req.body.password, user.password)
+
+  if (!valid) {
+    return res.status(401).json({ message: 'Invalid credentials' })
+  }
+
+  const token = createJWT(user)
+  res.json({ token })
+}
